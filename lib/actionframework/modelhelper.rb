5f7fa7754d398efa6e897a2e6f793a5b7156adb6
bvandenbogaerde <bram.vandenbogaerde@gmail.com>
2013-11-26 21:37:44 +0100

    Fixed ModelHelper

--- lib/actionframework/modelhelper.rb
+++ lib/actionframework/modelhelper.rb
@@ -1,18 +1,18 @@
 module ActionFramework
 	class ModelHelper
-		def post model,res
+		def self.post model,res
 			response = model.create(JSON.parse(req.body.string)).to_json
 			res.write response
 			res.finish
 		end
 
-		def get model,res
+		def self.get model,res
 			response = model.all.to_json
 			res.write response
 			res.finish
 		end
 
-		def update model,res
+		def self.update model,res
 			doc = JSON.parse(req.body.string)
 			modelfind = model.where(doc[:where])
 			response = modelfind.update_attributes(doc[:attributes]).to_json						
