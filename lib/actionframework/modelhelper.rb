5369790a9570b63c1b9d82c2ba4a0023d5e96e81
bvandenbogaerde <bram.vandenbogaerde@gmail.com>
2013-12-18 19:29:57 +0100

    auto-api security

--- lib/actionframework/modelhelper.rb
+++ lib/actionframework/modelhelper.rb
@@ -1,22 +1,34 @@
 module ActionFramework
 	class ModelHelper
 		def self.post model,res
-			response = model.create(JSON.parse(req.body.string)).to_json
-			res.write response
+			if(model.create?)
+				response = model.create(JSON.parse(req.body.string)).to_json
+				res.write response	
+			else
+				res.write {:error => "403 Forbidden"}.to_json
+			end
 			res.finish
 		end
 
 		def self.get model,res
-			response = model.all.to_json
-			res.write response
+			if(model.fetch?)
+				response = model.all.to_json
+				res.write response
+			else
+				res.write {:error => "403 Forbidden"}.to_json
+			end
 			res.finish
 		end
 
 		def self.update model,res
-			doc = JSON.parse(req.body.string)
-			modelfind = model.where(doc[:where])
-			response = modelfind.update_attributes(doc[:attributes]).to_json						
-			res.write response
+			if(model.update?)
+				doc = JSON.parse(req.body.string)
+				modelfind = model.where(doc[:where])
+				response = modelfind.update_attributes(doc[:attributes]).to_json						
+				res.write response
+			else
+				res.write {:error => "403 Forbidden"}.to_json
+			end
 			res.finish
 		end
 	end
