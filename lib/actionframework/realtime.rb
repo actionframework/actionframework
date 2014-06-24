ffbf15fd026945bea587325a76859424ff026883
Bram Vandenbogaerde <git.bramvdb@gmail.com>
2014-06-24 20:44:51 +0200

    let's finish up the redis integration with the realtime class

--- lib/actionframework/realtime.rb
+++ lib/actionframework/realtime.rb
@@ -2,20 +2,22 @@
 # Licenced under MIT ##
 ### © BramVDB.com #####
 #######################
+
 require 'rack/websocket'
 require 'socket'
 require 'json'
 require 'redis'
 
+$redis = Redis.new
 module ActionFramework
 	class Realtime < Rack::WebSocket::Application
 		def on_open(env)
 			return unless $realtime_config.enabled
 			request = Rack::Request.new(env)
-			redis = Redis.new
-			redis.publish request.path, env["HTTP_SEC_WEBSOCKET_KEY"]
 
-			redis.subscribe env["HTTP_SEC_WEBSOCKET_KEY"] do |on|
+			$redis.publish request.path, env["HTTP_SEC_WEBSOCKET_KEY"]
+
+			$redis.subscribe env["HTTP_SEC_WEBSOCKET_KEY"] do |on|
 				on.message do |channel,message|
 					send_data message
 				end
@@ -23,7 +25,7 @@ module ActionFramework
 		end
 
 		def on_message(env,message)
-
+			$redis.publish env["HTTP_SEC_WEBSOCKET_KEY"], message
 		end
 
 		def on_close(env)
@@ -39,5 +41,8 @@ module ActionFramework
 
 	class RealtimeConfigure
 		attr_accessor :enabled
+		## Redis might not fit your needs, that's why I will make something that your can plug your own server of choice into the websockets by using the predefined callbacks
+		attr_accessor :redis_prefix
+		attr_accessor :redis_uri
 	end
 end
