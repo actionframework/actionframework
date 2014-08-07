3cd33d3f5d3cd4c15fbb060b98d7430c8c91652c
Bram Vandenbogaerde <git.bramvdb@gmail.com>
2014-08-07 18:32:59 +0200

    removed unused code in TemplateManager

--- lib/actionframework/template_manager.rb
+++ lib/actionframework/template_manager.rb
@@ -3,13 +3,8 @@ module ActionFramework
 
     class Erb
       def self.render(template,ctx)
-        class << ctx
-            public :eval
-        end
-
         renderer = Tilt::ERBTemplate.new("views/layout.html.erb");
         output = renderer.render(ctx){ Tilt::ERBTemplate.new("views/"+template.to_s+".html.erb").render(ctx) }
-
       end
     end
 
